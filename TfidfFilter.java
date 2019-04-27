47595188cc0dd6be766acf9809798842346329c2
XIAYAN JI <xjiae@dc1.base.seas.upenn.edu>
2019-04-27 16:12:23 -0400

    xiayan

--- TfidfFilter.java
+++ TfidfFilter.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 public class TfidfFilter implements ITfidfFilter{
 
 	@Override
-	public HashMap<String, Integer> calculateTfidf(IDataBase db, FeedMessage msg) {
+	public HashMap<String, Double> calculateTfidf(DataBase db, FeedMessage msg) {
 		// TODO Auto-generated method stub
 		return null;
 	}
