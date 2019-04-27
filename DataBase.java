09926b0375369332ef6fcdd62f35ae7f1ba56d79
XIAYAN JI <xjiae@dc1.base.seas.upenn.edu>
2019-04-27 17:01:56 -0400

    xiayan database

--- DataBase.java
+++ DataBase.java
@@ -1,22 +1,31 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-public class DataBase implements IDataBase{
+import com.sun.java_cup.internal.runtime.Scanner;
+
+public class DataBase implements IDataBase {
 	final int MAX_HEAP_NUM = 100;
 	HashMap<String, MaxHeap> db;
 	int dbSize = 0;
-	
+	HashMap<String, Integer> allTermMap;
+
 	public DataBase() {
 		db = new HashMap<>();
+		allTermMap = new HashMap<>();
 	}
+
 	@Override
 	public List<FeedMessage> fetch(String keyWord) {
 		// TODO Auto-generated method stub
-		if (!db.keySet().contains(keyWord)) return null;
+		if (!db.keySet().contains(keyWord))
+			return null;
 		ArrayList<FeedMessage> res = new ArrayList<>();
-		for (String e : db.keySet()){
-			if (e.equals(keyWord)){
+		for (String e : db.keySet()) {
+			if (e.equals(keyWord)) {
 				// keyword found, return the maxHeap in the form of a list
 				MaxHeap heap = db.get(e);
 				res = heap.toList();
@@ -28,25 +37,36 @@ public class DataBase implements IDataBase{
 	@Override
 	public int store(Feed feed) {
 		// TODO Auto-generated method stub
-		if (feed == null) return -1;
+		if (feed == null)
+			return -1;
 		List<FeedMessage> msgList = feed.getMessages();
-		
-		for(FeedMessage msg: msgList){
+
+		for (FeedMessage msg : msgList) {
 			HashMap<String, Double> map = msg.getMap();
-			for(String s : map.keySet()){
+			for (String s : map.keySet()) {
 				MaxHeap heap = null;
-				if (db.keySet().contains(s)){
+				if (db.keySet().contains(s)) {
 					heap = db.get(s);
-					
-				}else{
-					FeedMessage[] h = {msg};
-					heap = new MaxHeap( h, h.length,  MAX_HEAP_NUM, s);
+
+				} else {
+					FeedMessage[] h = { msg };
+					heap = new MaxHeap(h, h.length, MAX_HEAP_NUM, s);
 				}
 				heap.insert(msg);
 				db.put(s, heap);
-					
+
 			}
-			
+			String text = msg.getNewsText();
+			String[] arr = text.split(" ");
+			Set<String> set = new HashSet<>(Arrays.asList(arr));
+			for (String s : set) {
+				int count = 0;
+				if (allTermMap.containsKey(s)) {
+					count = allTermMap.get(s);
+				}
+				count++;
+				allTermMap.put(s, count);
+			}
 		}
 		dbSize += feed.getMessages().size();
 		return feed.getMessages().size();
